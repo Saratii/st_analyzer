f3187476288ae9de4cff33e33d5163e07d626ae1
Saratii <friedchicken14821@gmail.com>
2024-07-11 15:58:02 -0500

    fix spelling error

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-class Catagory {
+class Category {
 	public color: vscode.TextEditorDecorationType;
 	public regex: RegExp;
 	public decoration: vscode.DecorationOptions[] = [];
@@ -16,28 +16,28 @@ class Catagory {
 export function activate(context: vscode.ExtensionContext) {
 	let timeout: NodeJS.Timeout | undefined = undefined;
 	let activeEditor = vscode.window.activeTextEditor;
-	const bools = new Catagory('#DF69BA', /\W(true|false)\W/g); //purple
-	const functions = new Catagory('#DFA000', /\b\w+(?=\s*\()/g); //yellow
-	const operators = new Catagory('#F57D26', /[+\-/*!=]/g); //orange
-	const comments = new Catagory('#8DA101', /\/\/.*/g); //green
-	const keywords = new Catagory('#F85552', new RegExp(['while', 'if', "for"].join('|'), 'g')); //red
-	const types = new Catagory('#3A94C5', new RegExp(/\b(i64|i32|Array|<|>)\b/g)); //blue
-	const catagories = [bools, functions, operators, comments, keywords, types];
+	const bools = new Category('#DF69BA', /\W(true|false)\W/g); //purple
+	const functions = new Category('#DFA000', /\b\w+(?=\s*\()/g); //yellow
+	const operators = new Category('#F57D26', /[+\-/*!=]/g); //orange
+	const comments = new Category('#8DA101', /\/\/.*/g); //green
+	const keywords = new Category('#F85552', new RegExp(['while', 'if', "for"].join('|'), 'g')); //red
+	const types = new Category('#3A94C5', new RegExp(/\b(i64|i32|Array|<|>)\b/g)); //blue
+	const categories = [bools, functions, operators, comments, keywords, types];
 	function updateDecorations() {
 		if (!activeEditor) {
 			return;
 		}
 		const text = activeEditor.document.getText();
-		for (const catagory of catagories) {
+		for (const category of categories) {
 			let match;
-			catagory.decoration = [];
-			while ((match = catagory.regex.exec(text))) {
+			category.decoration = [];
+			while ((match = category.regex.exec(text))) {
 				const startPos = activeEditor.document.positionAt(match.index);
 				const endPos = activeEditor.document.positionAt(match.index + match[0].length);
 				const range = new vscode.Range(startPos, endPos);
-				catagory.decoration.push({ range });
+				category.decoration.push({ range });
 			}
-			activeEditor.setDecorations(catagory.color, catagory.decoration);
+			activeEditor.setDecorations(category.color, category.decoration);
 		}
 	}
 
